0f82656555d8a539dfa8bf06a96b67c8c92ae260
Mihail <gogol251@mail.ru>
2021-07-23 19:48:05 +0300

    Сверстал шапку сайта

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,8 @@
 'use strict';
-const headerWrapElement = document.querySelector('.header__wrap--tablet');
+const headerWrapElement = document.querySelector('.header__wrap');
 const headerMobileMenuElement = headerWrapElement.querySelector('.header__mobile-menu');
 const headerButtonToggleElement = headerWrapElement.querySelector('.header__button-toggle');
+const bodyElement = document.querySelector('body');
 
 headerWrapElement.classList.remove('header__wrap--nojs');
 headerWrapElement.classList.remove('header__wrap--open');
@@ -11,8 +12,11 @@ headerButtonToggleElement.addEventListener('click', () => {
   if (headerWrapElement.classList.contains('header__wrap--open')) {
     headerWrapElement.classList.remove('header__wrap--open');
     headerMobileMenuElement.classList.remove('header__mobile-menu--open')
+    bodyElement.classList.remove('body--menu');
   } else {
     headerWrapElement.classList.add('header__wrap--open');
     headerMobileMenuElement.classList.add('header__mobile-menu--open')
+    headerWrapElement.classList.add('header__wrap--menu');
+    bodyElement.classList.add('body--menu');
   }
 });
